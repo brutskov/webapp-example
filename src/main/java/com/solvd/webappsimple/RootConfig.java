73fcb885150fae2ef7310f38c25ae86538546900
Bogdan Rutskov <rutkovba@gmail.com>
2019-08-07 20:54:00 +0300

    some fixes

--- src/main/java/com/solvd/webappsimple/RootConfig.java
+++ src/main/java/com/solvd/webappsimple/RootConfig.java
@@ -2,7 +2,7 @@ package com.solvd.webappsimple;
 
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan("com.solvd.qaprosofttosolvdrenamingspring")
+@ComponentScan("com.solvd.webappsimple")
 public class RootConfig {
 
 }
